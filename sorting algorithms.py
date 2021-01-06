640eb4796d221b77a207f6d1a2800872b87acfba
AntoineGS <antoineg.simard@gmail.com>
2021-01-05 22:01:16 -0500

    Remove new folder, will merge all sort algorithms in a single unit instead

--- sorting algorithms.py
+++ sorting algorithms.py
@@ -1,8 +1,7 @@
-# Basic implementation of selection sor for an array
-
 numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0]
 
 
+# Basic implementation of selection sor for an array
 def selection_sort(array):
     for i in range(len(array)):
         # Until proven otherwise i is the min (used to both initialize the values and skip the first iteration
@@ -20,4 +19,16 @@ def selection_sort(array):
     return array
 
 
+def bubble_sort(array):
+    for max_index in reversed(range(len(numbers))):
+        for curr_index in range(max_index):
+            if array[curr_index] > array[curr_index + 1]:
+                temp_value = array[curr_index]
+                array[curr_index] = array[curr_index + 1]
+                array[curr_index + 1] = temp_value
+
+    return array
+
+
 print(selection_sort(numbers))
+print(bubble_sort(numbers))
